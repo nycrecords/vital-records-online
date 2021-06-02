cc50914a834a7169c011cb1ff1e3fd0db3acd3ce
johnyu95 <jyu@records.nyc.gov>
2021-06-02 14:56:16 -0400

    Created db schema for vro

---
+++ vro/constants/certificate_types.py
@@ -0,0 +1,6 @@
+BIRTH = "birth"
+DEATH = "death"
+MARRIAGE = "marriage"
+MARRIAGE_LICENSE = "marriage_license"
+
+ALL = frozenset((BIRTH, DEATH, MARRIAGE, MARRIAGE_LICENSE))
---
+++ vro/models.py
@@ -0,0 +1,33 @@
+"""
+Models for Vital Records Online
+"""
+
+from vro.database import db
+from vro.constants import (
+    certificate_types,
+    counties
+)
+
+class Certificate(db.Model):
+    """
+    Define the Certificate class for the `certificates` table with the following columns:
+
+    id          integer, primary key
+    type        certificate_type, type of certificate (e.g. "birth")
+    county      county, certificate county (e.g. "queens")
+    year        integer, year certificate was issued
+    number      varchar(10), certificate number
+    """
+    __tablename__ = "certificates"
+
+    id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.Enum(*certificate_types.ALL, name='certificate_type'), nullable=False)
+    county = db.Column(db.Enum(*counties.ALL, name='county'), nullable=False)
+    year = db.Column(db.Integer)
+    number = db.Column(db.String(10))
+
+    def __init__(self, type, county, year, number):
+        self.type = type
+        self.county = county
+        self.year = year
+        self.number = number
