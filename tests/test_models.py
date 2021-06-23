71ac2ded9048e4e05e5a3427e511eedccb15d3c1
Gary Zhou <gzhou@records.nyc.gov>
2021-06-23 11:26:31 -0400

    Update test_models and code style changes

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,10 +2,9 @@
 """Model unit tests."""
 import pytest
 
+from vro.constants import certificate_types, counties
 from vro.extensions import db
-
 from vro.models import Certificate
-
 from .factories import CertificateFactory
 
 
@@ -13,20 +12,21 @@ from .factories import CertificateFactory
 class TestCertificate:
     """Certificate tests."""
 
-    def test_query_by_id(self):
-        """Get certificate by ID."""
-        certificate = Certificate('birth', 'queens', 1995, '100')
-        db.session.add(certificate)
-        db.session.commit()
+    def test_year_is_nullable(self):
+        """Test null year."""
+        certificate = Certificate(type_=certificate_types.BIRTH, county=counties.QUEENS, year=None, number="00100")
+        certificate.save()
+        assert certificate.year is None
 
-        retrieved = Certificate.query.filter_by(id=certificate.id).one_or_none()
-        assert retrieved == certificate
-
+    def test_number_is_nullable(self):
+        """Test null number."""
+        certificate = Certificate(type_=certificate_types.BIRTH, county=counties.QUEENS, year=1995, number=None)
+        certificate.save()
+        assert certificate.number is None
 
     def test_certificate_factory(self):
         """Test certificate factory."""
         certificate = CertificateFactory()
-        db.session.add(certificate)
         db.session.commit()
         assert bool(certificate.type)
         assert bool(certificate.county)
--- vro/models.py
+++ vro/models.py
@@ -20,13 +20,13 @@ class Certificate(PkModel):
     """
     __tablename__ = "certificates"
 
-    type = db.Column(db.Enum(*certificate_types.ALL, name='certificate_type'), nullable=False)
-    county = db.Column(db.Enum(*counties.ALL, name='county'), nullable=False)
+    type = db.Column(db.Enum(*certificate_types.ALL, name="certificate_type"), nullable=False)
+    county = db.Column(db.Enum(*counties.ALL, name="county"), nullable=False)
     year = db.Column(db.Integer)
     number = db.Column(db.String(10))
 
-    def __init__(self, type, county, year, number):
-        self.type = type
+    def __init__(self, type_, county, year, number):
+        self.type = type_
         self.county = county
         self.year = year
         self.number = number
